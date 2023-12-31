d60ac95933b1e3d51000f0e96f7b7f2a1c75a663
Atharva-77 <atharva.shirode99@gmail.com>
2023-12-31 02:15:37 -0500

    Change API link

--- src/Reducers/constants/userConstants.js
+++ src/Reducers/constants/userConstants.js
@@ -1,7 +1,6 @@
-export const USER_LOGIN_REQUEST='USER_LOGIN_REQUEST'
-export const USER_LOGIN_SUCCESS='USER_LOGIN_SUCCESS'
-export const USER_LOGIN_FAILURE='USER_LOGIN_FAILURE'
+export const USER_LOGIN_REQUEST = "USER_LOGIN_REQUEST";
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_FAILURE = "USER_LOGIN_FAILURE";
 
-export const USER_LOGOUT='USER_LOGOUT'
-export const LINKURL='http://localhost:4000'
-
+export const USER_LOGOUT = "USER_LOGOUT";
+export const LINKURL = "https://be-social-folks.vercel.app";
